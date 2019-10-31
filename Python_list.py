17c4c172eaa331bb4ba178238ba5089d9659fdb3
Gunjan827 <44015021+Gunjan827@users.noreply.github.com>
2019-10-31 06:08:54 +0530

    Add files via upload

---
+++ Python_list.py
@@ -0,0 +1,69 @@
+#This program displays individual player's stats of cricket team.There is list
+# of different data fields and appropriate data types to represent each of them
+
+
+player1 = ["Shikhar Dhawan",33,128,44.62]
+print ("name = ",player1[0]," , Age = ",player1[1]," ,Matches = ",player1[2]," ,Average = ",player1[3] )
+
+
+player2 = ["Rohit Sharma",32,206,47.39]
+print ("name = ",player2[0]," , Age = ",player2[1]," ,Matches = ",player2[2]," ,Average = ",player2[3] )
+
+
+player3 = ["K. L. Rahul",27,14,34.30]
+print ("name = ",player3[0]," , Age = ",player3[1]," ,Matches = ",player3[2]," ,Average = ",player3[3] )
+
+
+player4 = ["Virat Kohli",30,227,59.57]
+print ("name = ",player4[0]," , Age = ",player4[1]," ,Matches = ",player4[2]," ,Average = ",player4[3] )
+
+
+player5 = ["M. S. Dhoni",37,341,50.72]
+print ("name = ",player5[0]," , Age = ",player5[1]," ,Matches = ",player5[2]," ,Average = ",player5[3] )
+
+
+player6 = ["Kedar Jadhav",34,59,43.48]
+print ("name = ",player6[0]," , Age = ",player6[1]," ,Matches = ",player6[2]," ,Average = ",player6[3] )
+
+
+player7 = ["Vijay Shankar",28,9,33.00]
+print ("name = ",player7[0]," , Age = ",player7[1]," ,Matches = ",player7[2]," ,Average = ",player7[3] )
+
+
+player8 = ["Dinesh Karthik",33,91,31.03]
+print ("name = ",player8[0]," , Age = ",player8[1]," ,Matches = ",player8[2]," ,Average = ",player8[3] )
+
+
+player9 = ["Hardik Pandya",25,45,29.94]
+print ("name = ",player9[0]," , Age = ",player9[1]," ,Matches = ",player9[2]," ,Average = ",player9[3] )
+
+
+player10 = ["Ravindra Jadeja",30,151,29.93]
+print ("name = ",player10[0]," , Age = ",player10[1]," ,Matches = ",player10[2]," ,Average = ",player10[3] )
+
+
+player11 = ["Bhuvneshwar Kumar",29,105,5.01]
+print ("name = ",player11[0]," , Age = ",player11[1]," ,Matches = ",player11[2]," ,Eco = ",player11[3] )
+
+
+player12 = ["Mohammed Shami",29,63,5.48]
+print ("name = ",player12[0]," , Age = ",player12[1]," ,Matches = ",player12[2]," ,Eco = ",player12[3] )
+
+
+player13 = ["Jasprit Bumrah",25,49,4.51]
+print ("name = ",player13[0]," , Age = ",player13[1]," ,Matches = ",player13[2]," ,Eco = ",player13[3] )
+
+
+player14 = ["Kuldeep Yadav",24,44,4.93]
+print ("name = ",player14[0]," , Age = ",player14[1]," ,Matches = ",player14[2]," ,Eco = ",player14[3] )
+
+
+player15 = ["Yuzvendra Chahal",28,41,4.89]
+print ("name = ",player15[0]," , Age = ",player15[1]," ,Matches = ",player15[2]," ,Eco = ",player15[3] )
+
+
+
+
+
+
+
